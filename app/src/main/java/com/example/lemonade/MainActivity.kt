f62a8b7c197082e4b5ccd8b9f078c5568b103b14
Kahvi <kahvi@students.unnes.ac.id>
2025-04-10 16:38:51 +0700

    Add clip to the Image compose and add rounded shape background to it

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,7 +28,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -111,21 +114,28 @@ fun MainContent(modifier: Modifier = Modifier) {
         Image(
             painter = painterResource(imageContent),
             contentDescription = imageDesc,
-            modifier = Modifier.clickable {
-                if (currStep == 1) {
-                    squeezeCurrCnt++
-                    if (squeezeCurrCnt >= squeezeCntTarget) {
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .clip(RoundedCornerShape(16.dp))
+                .background(
+                    color = MaterialTheme.colorScheme.tertiaryContainer,
+                    shape = RoundedCornerShape(16.dp)
+                )
+                .clickable {
+                    if (currStep == 1) {
+                        squeezeCurrCnt++
+                        if (squeezeCurrCnt >= squeezeCntTarget) {
+                            currStep++
+                            squeezeCurrCnt = 0
+                        }
+                    } else if (currStep == 0) {
                         currStep++
-                        squeezeCurrCnt = 0
+                        squeezeCntTarget = (2..4).random()
+                    } else {
+                        currStep++
                     }
-                } else if (currStep == 0) {
-                    currStep++
-                    squeezeCntTarget = (2..4).random()
-                } else {
-                    currStep++
+                    currStep = currStep.rem(4)
                 }
-                currStep = currStep.rem(4)
-            }
         )
         Spacer(modifier = Modifier.height(16.dp))
         Text(
