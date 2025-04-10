b2758f49d118b7fb116ac3d363ed015d0879fc3c
Kahvi <kahvi@students.unnes.ac.id>
2025-04-10 16:12:06 +0700

    Finished the app: Add TopBar and MainContent compose

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -4,25 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,21 +50,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LemonadeApp(modifier: Modifier = Modifier) {
-    Title()
+    Column {
+        TopBar()
+        MainContent()
+    }
 }
 
 @Composable
-fun Title(modifier: Modifier = Modifier) {
+fun TopBar(modifier: Modifier = Modifier) {
     Column {
         Box(
             contentAlignment = Alignment.Center,
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxWidth(
+)
                 .background(Color.Yellow)
                 .padding(WindowInsets.safeDrawing.asPaddingValues())
         ) {
             Text(
                 text = stringResource(R.string.app_name),
+                fontSize = 24.sp,
                 fontWeight = FontWeight.Bold,
                 modifier = Modifier.padding(top = 16.dp, bottom = 16.dp)
             )
@@ -63,8 +79,59 @@ fun Title(modifier: Modifier = Modifier) {
 
 @Composable
 fun MainContent(modifier: Modifier = Modifier) {
-    Column(modifier = Modifier.fillMaxSize()) {
+    var currStep by remember { mutableIntStateOf(0) }
+    var squeezeCntTarget by remember { mutableIntStateOf(1) }
+    var squeezeCurrCnt by remember { mutableIntStateOf(0) }
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        val imageContent = when (currStep) {
+            0 -> R.drawable.lemon_tree
+            1 -> R.drawable.lemon_squeeze
+            2 -> R.drawable.lemon_drink
+            else -> R.drawable.lemon_restart
+        }
 
+       val imageDesc = when (currStep) {
+           0 -> stringResource(R.string.lemon_tree)
+           1 -> stringResource(R.string.lemon)
+           2 -> stringResource(R.string.glass_of_lemonade)
+           else -> stringResource(R.string.empty_glass)
+       }
+
+        val textContent = when (currStep) {
+            0 -> stringResource(R.string.step1)
+            1 -> stringResource(R.string.step2)
+            2 -> stringResource(R.string.step3)
+            else -> stringResource(R.string.step4)
+        }
+
+        Image(
+            painter = painterResource(imageContent),
+            contentDescription = imageDesc,
+            modifier = Modifier.clickable {
+                if (currStep == 1) {
+                    squeezeCurrCnt++
+                    if (squeezeCurrCnt >= squeezeCntTarget) {
+                        currStep++
+                        squeezeCurrCnt = 0
+                    }
+                } else if (currStep == 0) {
+                    currStep++
+                    squeezeCntTarget = (2..4).random()
+                } else {
+                    currStep++
+                }
+                currStep = currStep.rem(4)
+            }
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = textContent,
+            fontSize = 18.sp,
+        )
     }
 }
 
